79e0ea84aaea391af1aa5cb57b5642194f48c54c
Adam Lindley <johadalin@hotmail.com>
2025-05-22 12:33:03 +0100

    fix script issue with non titled webpages

--- script.py
+++ script.py
@@ -6,12 +6,29 @@ import re
 import csv
 import requests
 from bs4 import BeautifulSoup
+import logging
 
 CSV_PATH = "biglist.csv"
 #ANKI_MEDIA = ".local/share/Anki2/User 1/collection.media/"
 ANKI_MEDIA = "bsl-gcse/media/"
 ANKI_CSV = "bsl-gcse/csv/"
 
+logFileFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+fileHandler = logging.FileHandler("output.log")
+fileHandler.setFormatter(logFileFormatter)
+
+#logStreamFormatter = logging.Formatter("[%(levelname)-4.4s]  %(message)s")
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFileFormatter)
+
+logging.basicConfig(level=logging.DEBUG,
+                    handlers=[
+                        fileHandler,
+                        consoleHandler
+                        ]
+                    )
+
+logger = logging.getLogger(__name__)
 
 class Note:
     def __init__(self, *args):
@@ -47,6 +64,7 @@ class Note:
 
 def get_page(url):
     """Get the HTML for a given URL. Parse and return a BeautifulSoup object of it"""
+    logger.debug(f"getting page for url: {url}")
     html = requests.get(url).text
     page = BeautifulSoup(html, "lxml")
     return page
@@ -65,20 +83,20 @@ def get_definitions(url, tags):
     page = get_page(url)
     notes = []
     headings = [page.find("h1")] + page.find_all("h2")
-    #print(f"URL: {url} found headings h1 {page.find('h1')} and h2 {page.find_all('h2')}")
+    logger.debug(f"URL: {url} found headings h1 {page.find('h1')} and h2 {page.find_all('h2')}")
     if not page.find_all(itemprop="video"):
         raise LookupError(f"No video found on page {url}")
     video_count = len(page.find_all(itemprop='video'))
-    print(f"Findall videoprop = {video_count}")
+    logger.debug(f"Findall videoprop = {video_count}")
     for heading in headings:
         headword = heading.text
         tags += [x.text for x in heading.find_next_siblings("span")]
-        #print(f"tags: {tags}")
+        logger.debug(f"tags: {tags}")
         first_p = heading.find_next_sibling("p")
-        #print(f"first_p {first_p}")
+        logger.debug(f"first_p {first_p}")
         def_string = re.findall("</b> (.*?)<br/>", str(first_p))
         if def_string:
-            #print(f"have def_string {def_string}")
+            logger.debug(f"have def_string {def_string}")
             definition = def_string[0]
         else:
             definition = ""
@@ -88,14 +106,19 @@ def get_definitions(url, tags):
         else:
             example = ""
         video_div = first_p.find_next(itemprop="video")
-        #print(f"video_div {video_div}")
-        #print(f"video count: {first_p.find_all(itemprop='video')}")
+        logger.debug(f"video_div {video_div}")
         video_url = video_div.find(itemprop="contentURL")["content"]
-        video_title = re.findall("(<i>.*?) <br/>", str(video_div))[0]
+
+        video_title_strings = re.findall("(<i>.*?) <br/>", str(video_div))
+        if video_title_strings:
+            video_title = video_title_strings[0]
+        else:
+            logger.error("No video title found at url {video_url}. Using headword {headword}")
+            video_title = headword
+        logger.debug(f"video content url: {video_url}. title: {video_title}")
         notes.append(
             Note([headword, definition, example, video_url, video_title, url, str(video_count), tags])
         )
-    #print("\n\n\n")
     return notes
 
 
@@ -138,7 +161,7 @@ def word_list():
             for x in category.find_parent().find_all("a")
         ]
         for page in pages:
-            print("Contacting ", page, category.text)
+            logger.info("Contacting ", page, category.text)
             notes += get_definitions(page, [category.text])
     write_csv(CSV_PATH, notes)
 
@@ -156,7 +179,7 @@ def convert_video(url, dest):
     filename = video_filename(url)
     #dest = Path.home() / Path(ANKI_MEDIA)
     if exists(dest / filename):
-        #print("Already converted", dest / filename)
+        logger.debug(f"Already converted {dest}/{filename}")
         return
     response = requests.get(url)
     with open(f"{temp / filename}", mode="wb") as file:
@@ -204,8 +227,9 @@ def add_signs(signs, tags, output):
         try:
             note = get_definitions("https://www.signbsl.com/sign/" + escape_spaces(sign), tags)
             notes += note
-        except LookupError:
-            print(f"No video found for sign {sign}. Adding to error list")
+        except LookupError as e:
+            logger.exception("Caught lookup error")
+            logger.warning(f"No video found for sign {sign}. Adding to error list")
             errors.append(sign)
     write_csv(output, notes)
     return errors
@@ -219,11 +243,11 @@ if __name__ == '__main__':
     csv_file = csv_path / Path(args.filename + '.csv')
 
     word_list = []
-    print(f"Parsing file {args.filename}")
+    logger.info(f"Parsing file {args.filename}")
     with open(args.filename,'r') as f:
         word_list.extend(line.strip('\n') for line in f.readlines())
     errors = add_signs(word_list, [], csv_file)
     download_videos(csv_file, ANKI_MEDIA + args.filename)
-    print(f"Videos for the following signs were not found:\n{errors}")
+    logger.warning(f"Videos for the following signs were not found:\n{errors}")
     non_starred_errors = [word for word in errors if '*' not in word]
-    print(f"Non starred errors:\n{non_starred_errors}")
+    logger.warning(f"Non starred errors:\n{non_starred_errors}")
